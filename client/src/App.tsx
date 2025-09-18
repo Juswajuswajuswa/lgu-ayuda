ed3e32891faa84f80f22b1b98a48248942f95c53
Ginric Alayon <ginric@thehowdystudios.com>
2025-09-18 22:45:55 +0800

    Setup Tailwind CSS and Shadcn UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState<number>(0);
+  const handleClick = () => {
+    alert("Button clicked");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="min-h-screen flex flex-col gap-4 bg-gray-100 items-center justify-center">
+        <h1 className="text-3xl font-bold underline">LGU Ayuda Distribution</h1>
+        <Button variant="outline" onClick={handleClick}>
+          Click me
+        </Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
