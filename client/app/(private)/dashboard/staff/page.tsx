5fec524c02cd4d76b5f3edd38e7d22485abb74a7
Ginric Alayon <ginric@thehowdystudios.com>
2025-10-12 14:34:01 +0800

    asdas

--- client/app/(private)/dashboard/staff/page.tsx
+++ client/app/(private)/dashboard/staff/page.tsx
@@ -13,13 +13,8 @@ import { Loader2, PlusIcon } from "lucide-react";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
 import Link from "next/link";
-<<<<<<< HEAD
 import axiosInstance from "@/lib/axios";
 import { useQuery } from "@tanstack/react-query";
-=======
-import { useQuery } from "@tanstack/react-query";
-import axiosInstance from "@/lib/axios";
->>>>>>> 8c1449c63ed0650a2483e4da0391ef25936fbf8b
 
 export default function StaffPage() {
   const {
@@ -29,28 +24,10 @@ export default function StaffPage() {
   } = useQuery({
     queryKey: ["staffs"],
     queryFn: async () => {
-      const res = await axiosInstance.get("/user/get-staffs");
-      return res.data;
-    },
-<<<<<<< HEAD
-  });
-=======
-  ];
-}
-
-export default function StaffPage() {
-  // const data = await getData();
-
-  const { data: staffs } = useQuery({
-    queryKey: ["staffs"],
-    queryFn: async () => {
-      const res = await axiosInstance.get(`/user/get-users`);
+      const res = await axiosInstance.get("/user/get-users");
       return res.data;
     },
   });
-
-  console.log(staffs);
->>>>>>> 8c1449c63ed0650a2483e4da0391ef25936fbf8b
 
   return (
     <>
@@ -69,7 +46,6 @@ export default function StaffPage() {
           </CardAction>
         </CardHeader>
         <CardContent>
-<<<<<<< HEAD
           {isPending ? (
             <div className="h-24 flex items-center justify-center gap-2">
               <Loader2 className="w-4 h-4 animate-spin" />
@@ -82,9 +58,6 @@ export default function StaffPage() {
           ) : (
             <DataTable columns={columns} data={staffs.users || []} />
           )}
-=======
-          {/* <DataTable columns={columns} data={data} /> */}
->>>>>>> 8c1449c63ed0650a2483e4da0391ef25936fbf8b
         </CardContent>
       </Card>
     </>
